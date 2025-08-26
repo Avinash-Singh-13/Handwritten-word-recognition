261ee69de2351fbfa547833aa812301ad3127127
Avinash Singh <2k22.cse.2211369@gmail.com>
2025-08-26 11:24:40 +0530

    Update infer.py

--- scripts/infer.py
+++ scripts/infer.py
@@ -10,7 +10,6 @@ def infer(image_path, model_dir, model_name="microsoft/trocr-small-handwritten")
     pixel_values = proc(img, return_tensors="pt").pixel_values.to(model.device)
     outputs = model.generate(pixel_values)
     return proc.batch_decode(outputs, skip_special_tokens=True)[0]
-
 if __name__ == "__main__":
     img_path = "path/to/sample.jpg"
     print("Prediction:", infer(img_path, "models/trocr_handwritten/checkpoint-xxx"))
